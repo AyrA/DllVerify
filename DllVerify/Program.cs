23fb69aeddea0d632d2b1f5bbf7a21c3217a1241
Kevin Gut <kevin.gut@bitm.sg>
2021-06-24 22:08:05 +0200

    Add project files.

---
+++ DllVerify/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DllVerify
+{
+    public class Program
+    {
+        public const int EXIT_FAILED = -1;
+        public const int EXIT_SUCCESS = 0;
+
+        private class Arguments
+        {
+            public string FileName { get; private set; }
+            public bool Help { get; private set; }
+            public bool Verbose { get; private set; }
+            public bool NoArgs { get; private set; }
+            public bool Search { get; private set; }
+
+            public Arguments(string[] args)
+            {
+                if (args == null || args.Length == 0)
+                {
+                    NoArgs = true;
+                }
+                else
+                {
+                    foreach (var a in args)
+                    {
+                        switch (a.ToLower())
+                        {
+                            case "/?":
+                                if (Help)
+                                {
+                                    throw new ArgumentException($"Duplicate argument: {a.ToUpper()}");
+                                }
+                                Help = true;
+                                break;
+                            case "/v":
+                                if (Verbose)
+                                {
+                                    throw new ArgumentException($"Duplicate argument: {a.ToUpper()}");
+                                }
+                                Verbose = true;
+                                break;
+                            case "/s":
+                                if (Search)
+                                {
+                                    throw new ArgumentException($"Duplicate argument: {a.ToUpper()}");
+                                }
+                                Search = true;
+                                break;
+                            default:
+                                if (FileName == null)
+                                {
+                                    FileName = a;
+                                }
+                                else
+                                {
+                                    throw new ArgumentException($"Unknown argument and file name already set: \"{a}\"");
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        static int Main(string[] args)
+        {
+            Arguments A;
+            try
+            {
+                A = new Arguments(args);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Unable to parse command line arguments. Reason:\r\n{0}", ex.Message);
+                Console.Error.WriteLine("Use /? for help");
+                return EXIT_FAILED;
+            }
+            if (A.NoArgs || A.Help)
+            {
+                Console.WriteLine(@"{0} <DllFile> [/V] [/S]
+Checks if the given DLL file or executable has a valid signature.
+
+/V   Verbose output
+       Shows final DLL name when searching
+       Shows search locations and exit codes when requesting help
+/S   Search system for DLL file
+       Search locations in order of preference:
+       1. Application path
+       2. Windows System32 directory
+       3. Windows System directory
+       4. Windows directory
+       5. Current working directory (%CD%)
+       6. Directories in the path variable (%PATH%)
+
+Note: Searching will subject the file name to name changes,
+similar to how Windows would do it.
+Primarily, this means it adds '.dll' if it's not there.
+
+Possible exit codes:
+--------------------", GetProcName());
+                var V = Enum.GetValues(typeof(Trust.WinVerifyTrustResult))
+                    .OfType<Trust.WinVerifyTrustResult>()
+                    .OrderBy(m => ToExit(m))
+                    .ToArray();
+                var Max = V.Max(m => m.ToString().Length);
+                foreach (var E in V)
+                {
+                    Console.WriteLine("{0,-" + Max + "}: {1}", E, ToExit(E));
+                }
+                if (A.Verbose)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Search order (may contain duplicates):");
+                    Console.WriteLine(string.Join("\n", GetDllSearchPath()));
+                }
+                return EXIT_SUCCESS;
+            }
+            else
+            {
+                try
+                {
+                    string RealName;
+                    if (A.Search)
+                    {
+                        RealName = FindFullDllName(A.FileName);
+                    }
+                    else
+                    {
+                        RealName = Path.GetFullPath(A.FileName);
+                    }
+                    if (A.Verbose)
+                    {
+                        Console.WriteLine(RealName);
+                    }
+                    return ToExit(CheckDLL(RealName));
+                }
+                catch (FileNotFoundException)
+                {
+                    if (A.Search)
+                    {
+                        Console.Error.WriteLine("The given file could not be found in the search locations");
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine($"The given file could not be found: {A.FileName}");
+                    }
+                }
+            }
+            return ToExit(Trust.WinVerifyTrustResult.ObjectNotFound);
+        }
+
+        /// <summary>
+        /// Converts a verify result to an easier to manage exit code
+        /// </summary>
+        /// <param name="Result">DLL verify result</param>
+        /// <returns>Exit code</returns>
+        /// <remarks>
+        /// Works as long as every value is unique
+        /// </remarks>
+        private static int ToExit(Trust.WinVerifyTrustResult Result)
+        {
+            return (int)Result & 0xFFFF;
+        }
+
+        /// <summary>
+        /// Checks if the given DLL file name (or any executable) has a valid signature.
+        /// </summary>
+        /// <param name="DllFileName">DLL or executable file</param>
+        /// <returns>Verification result</returns>
+        private static Trust.WinVerifyTrustResult CheckDLL(string DllFileName)
+        {
+            return Trust.WinTrust.VerifyEmbeddedSignature(DllFileName);
+        }
+
+        /// <summary>
+        /// Tries to find the full path of a given DLL file using the system default search order.
+        /// If the supplied argument is a rooted path, it's used as-is.
+        /// </summary>
+        /// <param name="DllFile">DLL file name</param>
+        /// <returns>Full path</returns>
+        /// <exception cref="FileNotFoundException">Thrown if none of the searched paths contains the file name</exception>
+        /// <remarks>
+        /// This cannot handle a custom search path (those added via AddDllDirectory)
+        /// and will only search in the system default order.
+        /// Due to overlaps, this may search some locations twice.
+        /// Order:
+        /// 1. Application directory
+        /// 2. Windows System32 directory
+        /// 3. Windows System directory
+        /// 4. Windows directory
+        /// 5. Current Path (%CD%)
+        /// 6. All directories in %PATH%
+        /// </remarks>
+        private static string FindFullDllName(string DllFile)
+        {
+            var Dirs = GetDllSearchPath();
+            //Windows adds ".dll" if the extension is not present and the name doesn't ends with a dot
+            if (!DllFile.ToLower().EndsWith(".dll"))
+            {
+                if (!DllFile.EndsWith("."))
+                {
+                    DllFile += ".dll";
+                }
+                else
+                {
+                    DllFile = DllFile.Substring(0, DllFile.Length - 1);
+                }
+            }
+            if (Path.IsPathRooted(DllFile) && File.Exists(DllFile))
+            {
+                return Path.GetFullPath(DllFile);
+            }
+            var RealPath = Dirs.FirstOrDefault(m => File.Exists(Path.Combine(m, DllFile)));
+            if (RealPath != null)
+            {
+                return Path.Combine(RealPath, DllFile);
+            }
+            var EX = new FileNotFoundException($"{DllFile} doesn't exists in the DLL search path. Check Exception data for directory list");
+            EX.Data.Add("SearchOrder", Dirs);
+            throw EX;
+        }
+
+        /// <summary>
+        /// Gets all system DLL search paths in the order they will be searched
+        /// </summary>
+        /// <returns>Array of directories</returns>
+        /// <remarks>This list may contain duplicates</remarks>
+        private static string[] GetDllSearchPath()
+        {
+            var Dirs = new List<string>();
+            Dirs.Add(Path.GetDirectoryName(GetProcPath()));
+            Dirs.Add(Environment.GetFolderPath(Environment.SpecialFolder.System));
+            Dirs.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "System"));
+            Dirs.Add(Environment.GetFolderPath(Environment.SpecialFolder.Windows));
+            Dirs.Add(Environment.CurrentDirectory);
+            foreach (var P in Environment.GetEnvironmentVariable("PATH").Split(';'))
+            {
+                Dirs.Add(P);
+            }
+            return Dirs.ToArray();
+        }
+
+        /// <summary>
+        /// Gets the executable file name of the current process
+        /// </summary>
+        /// <returns>Executable file name</returns>
+        private static string GetProcName()
+        {
+            return Path.GetFileName(GetProcPath());
+        }
+
+        /// <summary>
+        /// Gets the full path to the current executable file name
+        /// </summary>
+        /// <returns>full executable path</returns>
+        private static string GetProcPath()
+        {
+            using (var P = System.Diagnostics.Process.GetCurrentProcess())
+            {
+                return P.MainModule.FileName;
+            }
+        }
+    }
+}
---
+++ DllVerify/Verify.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DllVerify.Trust
+{
+    /// <summary>
+    /// Results of file signature verification
+    /// </summary>
+    public enum WinVerifyTrustResult : uint
+    {
+        /// <summary>
+        /// Signature is present and considered valid by the operating system
+        /// </summary>
+        Success = 0,
+        /// <summary>
+        /// Trust provider is not recognized on this system
+        /// </summary>
+        ProviderUnknown = 0x800b0001,
+        /// <summary>
+        /// Trust provider does not support the specified action
+        /// </summary>
+        ActionUnknown = 0x800b0002,
+        /// <summary>
+        /// Trust provider does not support the form specified for the subject
+        /// </summary>
+        SubjectFormUnknown = 0x800b0003,
+        /// <summary>
+        /// Subject failed the specified verification action
+        /// </summary>
+        SubjectNotTrusted = 0x800b0004,
+        /// <summary>
+        /// TRUST_E_NOSIGNATURE - File was not signed
+        /// </summary>
+        FileNotSigned = 0x800B0100,
+        /// <summary>
+        /// Signer's certificate is in the Untrusted Publishers store
+        /// </summary>
+        SubjectExplicitlyDistrusted = 0x800B0111,
+        /// <summary>
+        /// TRUST_E_BAD_DIGEST - file was probably corrupt
+        /// </summary>
+        SignatureOrFileCorrupt = 0x80096010,
+        /// <summary>
+        /// CERT_E_EXPIRED - Signer's certificate was expired
+        /// </summary>
+        SubjectCertExpired = 0x800B0101,
+        /// <summary>
+        /// CERT_E_REVOKED Subject's certificate was revoked
+        /// </summary>
+        SubjectCertificateRevoked = 0x800B010C,
+        /// <summary>
+        /// CERT_E_UNTRUSTEDROOT - A certification chain processed correctly but terminated in a root certificate that is not trusted by the trust provider.
+        /// </summary>
+        UntrustedRoot = 0x800B0109,
+        /// <summary>
+        /// The specified object was not found
+        /// </summary>
+        /// <remarks>This is not an official code</remarks>
+        ObjectNotFound = 0x80092003
+    }
+
+    /// <summary>
+    /// Checks file signatures
+    /// </summary>
+    public static class WinTrust
+    {
+        #region WinTrustData struct field enums
+        private enum WinTrustDataUIChoice : uint
+        {
+            All = 1,
+            None = 2,
+            NoBad = 3,
+            NoGood = 4
+        }
+
+        private enum WinTrustDataRevocationChecks : uint
+        {
+            None = 0x00000000,
+            WholeChain = 0x00000001
+        }
+
+        private enum WinTrustDataChoice : uint
+        {
+            File = 1,
+            Catalog = 2,
+            Blob = 3,
+            Signer = 4,
+            Certificate = 5
+        }
+
+        private enum WinTrustDataStateAction : uint
+        {
+            Ignore = 0x00000000,
+            Verify = 0x00000001,
+            Close = 0x00000002,
+            AutoCache = 0x00000003,
+            AutoCacheFlush = 0x00000004
+        }
+
+        [Flags]
+        private enum WinTrustDataProvFlags : uint
+        {
+            UseIe4TrustFlag = 0x00000001,
+            NoIe4ChainFlag = 0x00000002,
+            NoPolicyUsageFlag = 0x00000004,
+            RevocationCheckNone = 0x00000010,
+            RevocationCheckEndCert = 0x00000020,
+            RevocationCheckChain = 0x00000040,
+            RevocationCheckChainExcludeRoot = 0x00000080,
+            SaferFlag = 0x00000100,        // Used by software restriction policies. Should not be used.
+            HashOnlyFlag = 0x00000200,
+            UseDefaultOsverCheck = 0x00000400,
+            LifetimeSigningFlag = 0x00000800,
+            CacheOnlyUrlRetrieval = 0x00001000,      // affects CRL retrieval and AIA retrieval
+            DisableMD2andMD4 = 0x00002000      // Win7 SP1+: Disallows use of MD2 or MD4 in the chain except for the root
+        }
+
+        private enum WinTrustDataUIContext : uint
+        {
+            Execute = 0,
+            Install = 1
+        }
+        #endregion
+
+        #region WinTrust structures
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private class WinTrustFileInfo : IDisposable
+        {
+            uint StructSize = (uint)Marshal.SizeOf(typeof(WinTrustFileInfo));
+            IntPtr pszFilePath;                     // required, file name to be verified
+            IntPtr hFile = IntPtr.Zero;             // optional, open handle to FilePath
+            IntPtr pgKnownSubject = IntPtr.Zero;    // optional, subject type if it is known
+
+            public WinTrustFileInfo(string _filePath)
+            {
+                pszFilePath = Marshal.StringToCoTaskMemAuto(_filePath);
+            }
+            public void Dispose()
+            {
+                if (pszFilePath != IntPtr.Zero)
+                {
+                    Marshal.FreeCoTaskMem(pszFilePath);
+                    pszFilePath = IntPtr.Zero;
+                }
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private class WinTrustData : IDisposable
+        {
+            uint StructSize = (uint)Marshal.SizeOf(typeof(WinTrustData));
+            IntPtr PolicyCallbackData = IntPtr.Zero;
+            IntPtr SIPClientData = IntPtr.Zero;
+            // required: UI choice
+            WinTrustDataUIChoice UIChoice = WinTrustDataUIChoice.None;
+            // required: certificate revocation check options
+            WinTrustDataRevocationChecks RevocationChecks = WinTrustDataRevocationChecks.None;
+            // required: which structure is being passed in?
+            WinTrustDataChoice UnionChoice = WinTrustDataChoice.File;
+            // individual file
+            IntPtr FileInfoPtr;
+            WinTrustDataStateAction StateAction = WinTrustDataStateAction.Ignore;
+            IntPtr StateData = IntPtr.Zero;
+            string URLReference = null;
+            WinTrustDataProvFlags ProvFlags = WinTrustDataProvFlags.RevocationCheckChainExcludeRoot;
+            WinTrustDataUIContext UIContext = WinTrustDataUIContext.Execute;
+
+            // constructor for silent WinTrustDataChoice.File check
+            public WinTrustData(WinTrustFileInfo _fileInfo)
+            {
+                // On Win7SP1+, don't allow MD2 or MD4 signatures
+                if ((Environment.OSVersion.Version.Major > 6) ||
+                    ((Environment.OSVersion.Version.Major == 6) && (Environment.OSVersion.Version.Minor > 1)) ||
+                    ((Environment.OSVersion.Version.Major == 6) && (Environment.OSVersion.Version.Minor == 1) && !string.IsNullOrEmpty(Environment.OSVersion.ServicePack)))
+                {
+                    ProvFlags |= WinTrustDataProvFlags.DisableMD2andMD4;
+                }
+
+                WinTrustFileInfo wtfiData = _fileInfo;
+                FileInfoPtr = Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(WinTrustFileInfo)));
+                Marshal.StructureToPtr(wtfiData, FileInfoPtr, false);
+            }
+            public void Dispose()
+            {
+                if (FileInfoPtr != IntPtr.Zero)
+                {
+                    Marshal.FreeCoTaskMem(FileInfoPtr);
+                    FileInfoPtr = IntPtr.Zero;
+                }
+            }
+        }
+        #endregion
+
+        private static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
+        // GUID of the action to perform
+        private const string WINTRUST_ACTION_GENERIC_VERIFY_V2 = "{00AAC56B-CD44-11d0-8CC2-00C04FC295EE}";
+
+        [DllImport("wintrust.dll", ExactSpelling = true, SetLastError = false, CharSet = CharSet.Unicode)]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+        static extern WinVerifyTrustResult WinVerifyTrust(
+            [In] IntPtr hwnd,
+            [In][MarshalAs(UnmanagedType.LPStruct)] Guid pgActionID,
+            [In] WinTrustData pWVTData
+        );
+
+        /// <summary>
+        /// Verifies the file signature of an authenticode signed file
+        /// </summary>
+        /// <param name="fileName">File name and path</param>
+        /// <returns>Verification result</returns>
+        public static WinVerifyTrustResult VerifyEmbeddedSignature(string fileName)
+        {
+            using (WinTrustFileInfo wtfi = new WinTrustFileInfo(fileName))
+            {
+                using (WinTrustData wtd = new WinTrustData(wtfi))
+                {
+                    Guid guidAction = new Guid(WINTRUST_ACTION_GENERIC_VERIFY_V2);
+                    return WinVerifyTrust(INVALID_HANDLE_VALUE, guidAction, wtd);
+                }
+            }
+        }
+    }
+}
